837c071bb6898c11ea2412fe5cec10399b460aec
kilicseyit <seyit.kilic1@outlook.com>
2023-12-26 15:44:36 +0300

    WebApi
    Oluşturulan kayıt formuna girilen bilgileri indirme işlemlerini yapan Api

---
+++ webApi/webApi/Controllers/SampleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace webApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(IFormFile  formData)
+        { 
+            if (formData != null)
+            {
+                try
+                {
+                    // Veriyi JSON formatında metin dosyasına kaydetme
+                    string json = JsonSerializer.Serialize(formData);
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), "FormData.txt");
+                    await System.IO.File.WriteAllTextAsync(filePath, json);
+
+                    return Ok("Veri başarıyla kaydedildi.");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Hata oluştu: " + ex.Message);
+                }
+            }
+            else
+            {
+                return BadRequest("Geçersiz veri.");
+            }
+        }
+    }
+
+    public class FormData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // Diğer veri alanlarını buraya ekleyebilirsiniz.
+    }
+}
